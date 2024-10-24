e61ff7f984007a673bdae9461c754788ca6e0bd5
Matthew Hudson <matthew41305@gmail.com>
2024-10-23 19:15:38 -0700

    addded following prompt and the results

--- script.js
+++ script.js
@@ -13,3 +13,13 @@ console.log("Hello World!");
 console.log(8 * null); // result will be zero
 
 console.log(10 % 2 == 0 ? "even" : "odd");
+
+// lecture notes portion
+console.log(8 * null); // result: 0
+console.log("5" - 1); // result: 4
+console.log("5" + 1); // result: 51
+console.log("five" * 2); // result :NaN (not a number)
+console.log( false == 0); // result :true
+console.log(5 > 2 && 6 > 3); // result: true
+console.log(false == false ? "equal" : "not equal"); // result: equal
+console.log( true || false); // result: true
