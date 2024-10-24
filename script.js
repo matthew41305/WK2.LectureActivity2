4a487b91b376b472be54e3220159edc179b73b67
Matthew Hudson <matthew41305@gmail.com>
2024-10-23 19:00:01 -0700

    created a few console log lines showing different responses

---
+++ script.js
@@ -0,0 +1,15 @@
+// this is a comment line
+
+/*
+        this
+        is
+        a
+        multiline
+        comment
+*/
+
+console.log("Hello World!");
+
+console.log(8 * null); // result will be zero
+
+console.log(10 % 2 == 0 ? "even" : "odd");
